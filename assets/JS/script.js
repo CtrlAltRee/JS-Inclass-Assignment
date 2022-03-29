f16729c778aad24a8606a2ce073a0e76bd8a99ae
CtrlAltRee <dess.rucker@gmail.com>
2022-03-28 22:53:43 -0400

    js all box changes

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,8 @@
-let firstbox, secondbox, thirdbox, btn1, btn2, btn3, btnall, allColors, allBorders; 
+let firstbox, secondbox, thirdbox, btn1, btn2, btn3, btnall, allColors, allBorders, allPixels; 
 
 allColors = ["yellow", "lightgreen", "black", "salmon", "silver"]
-allBorders = ["groove", "dashed", "ridge", "dotted", "solid", "outset"]
+allBorders = ["double", "groove", "dashed", "ridge", "dotted", "solid", "outset"]
+allPixels = ["2.5px", "5px", "6.5px"]
 
 firstbox = document.getElementById('first');
 secondbox = document.getElementById('second');
@@ -24,4 +25,16 @@ btn1.addEventListener("click", changeBoxOne);
 // to round # down = Math.floor()
 // to round # up = Math.ceil()
 
+function changeBoxTwo() {
+    secondbox.style.backgroundColor = allColors[Math.floor(Math.random() * allColors.length)];
+    secondbox.style.border = allPixels[Math.floor(Math.random() * allPixels.length)] + " " + allBorders[Math.floor(Math.random() * allColors.length)] + " " + allColors[Math.floor(Math.random() * allColors.length)];
+}
 
+btn2.addEventListener("click", changeBoxTwo);
+
+function changeBoxThree() {
+    thirdbox.style.backgroundColor = allColors[Math.floor(Math.random() * allColors.length)];
+    thirdbox.style.border = "4px "+ allBorders[Math.floor(Math.random() * allColors.length)] + " " + allColors[Math.floor(Math.random() * allColors.length)];
+}
+
+btn3.addEventListener("click", changeBoxThree);
